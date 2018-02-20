66499645cf534b6623521396e046ce57544fbf28
Sha <mohansha@github.com>
2018-02-20 15:04:22 +0530

    added wolfram bot

---
+++ wolfram_bot.py
@@ -0,0 +1,35 @@
+import wolframalpha
+
+query=raw_input('Ask me something: ')
+app_id='API_KEY'
+
+
+client=wolframalpha.Client(app_id)
+
+def wolfram(query):
+    res=client.query(str(query))
+    try:
+       ans=next(res.results).text
+    except:
+       ans='Sorry, I don\'t know that.'
+    return ans
+
+print wolfram(query)
+
+q='y'
+while q=='y':
+   q=raw_input('Wanna ask another question(y/n)? ')
+   if q=='n':
+       break
+   elif q=='y':
+       query=raw_input('You: ')
+       print 'Bot: '+wolfram(query)
+   else:
+       print 'Invalid input. Please try again'
+       q='y'
+       
+       
+        
+    
+
+
